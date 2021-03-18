6f5f5758ec34a3191b61801e562c6dabc153cb13
b6320500697 <78530335+b6320500697@users.noreply.github.com>
2021-03-18 16:59:59 +0700

    ข้อ2เพิ่มเติม

--- 6320500697-2.c
+++ 6320500697-2.c
@@ -7,4 +7,13 @@ int main()
     int rm1[n];
     scanf("%d",&f);
     scanf("%d",&rm);
+    for(i=0;i<n;i++)
+    {
+        fi[i]=f%10;
+        f=f/10;
+        rm1[i]=rm%10;
+        rm=rm/10;
+    }
+
+
 }
