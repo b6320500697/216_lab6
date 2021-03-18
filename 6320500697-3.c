bd953083394546c5e8b3845b5a06160991b9f7c9
b6320500697 <78530335+b6320500697@users.noreply.github.com>
2021-03-18 17:43:01 +0700

    ข้อ3

--- 6320500697-3.c
+++ 6320500697-3.c
@@ -1,21 +1,40 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,k,n,a,b,c,max,d;
+    int i,j,k,n,a,b,c,max,d=0;
     scanf ("%d %d",&n,&a);
     char w;
     int q[n],e[n];
+
     for (i=0;i<n;i++)
     {
-        q[j] = 0;
-        e[j] = 0;
+        q[i] = 0;
+        e[i] = 0;
     }
-    for (j=0;j<q;j++)
+    for (i=0;i<a;i++)
     {
         scanf (" %c %d %d",&w,&b,&c);
-        if (w=='U')
+        if (w == 'U')
         {
             q[b-1] = c;
         }
+        else if (w == 'P')
+        {
+            max = q[b-1];
+            for (k=b;k<c;k++)
+            {
+                if (max<q[k])
+                {
+                    max=q[k];
+                }
+            }
+            e[d] = max;
+            d++;
+        }
     }
+    for (i=0;i<d;i++)
+    {
+        printf ("%d\n",e[i]);
+    }
+    return 0;
 }
