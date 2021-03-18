0242f4d56c2a6c7f7c97c88cf1089f74b5b32d1e
b6320500697 <78530335+b6320500697@users.noreply.github.com>
2021-03-18 15:33:14 +0700

    ข้อ1เพิ่มการเปรียบเทียบ

--- 632050097-1.c
+++ 632050097-1.c
@@ -2,12 +2,24 @@
 #include <string.h>
 int main ()
 {
-    int n,i;
+    int n,i,j,k;
     scanf ("%d",&n);
     char word[n][50];
-    char cah[50];
+    char cha[50];
     for (i=0;i<n;i++)
     {
         scanf ("%s",word[i]);
+    }
+    for(j=0;j<n;j++)
+    {
+        for(k=0;k<n;k++)
+        {
+            if(strcmp(word[k],word[k+1])==1)
+            {
+                strcpy(cha,word[k]);
+                strcpy(word[k],cha[k+1]);
+                strcpy(word[k+1],cha);
+            }
+        }
     }
 }
