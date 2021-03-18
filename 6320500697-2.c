a475bcb0380cf8dd71f36b8fa8094582ea90e6d2
b6320500697 <78530335+b6320500697@users.noreply.github.com>
2021-03-18 17:03:36 +0700

    ข้อ2ครบ

--- 6320500697-2.c
+++ 6320500697-2.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int n,i,rm,f,j,k;
+    int n,i,rm,f,j,k,d;
     scanf("%d",&n);
     int fi[n];
     int rm1[n];
@@ -14,6 +14,18 @@ int main()
         rm1[i]=rm%10;
         rm=rm/10;
     }
-
+    for(j=n-1;j>=0;j--)
+    {
+        if(fi[j]==rm1[j])
+        {
+            d++;
+        }
+        else
+        {
+            k++;
+        }
+    }
+    printf("%d %d",d,k);
+    return 0;
 
 }
