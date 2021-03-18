fbd0a60f28a27da6a6c1228bbee55b11395affa4
b6320500697 <78530335+b6320500697@users.noreply.github.com>
2021-03-18 15:08:22 +0700

    ข้อ1

---
+++ 632050097-1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main ()
+{
+    int n;
+    char cha[26]={'A','B','C','D','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    scanf ("%d",&n);
+    char cha1[n];
+}
