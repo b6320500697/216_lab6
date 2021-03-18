b0edd06a6c0d678391b0eebe33bdb3dbc0c8ce12
b6320500697 <78530335+b6320500697@users.noreply.github.com>
2021-03-18 16:24:19 +0700

    ข้อ1

--- 632050097-1.c
+++ 632050097-1.c
@@ -2,33 +2,23 @@
 #include <string.h>
 int main ()
 {
-    int n,i,j,k=0;
+    int n,i,j;
     scanf ("%d",&n);
     char word[n][50];
     char cha[50];
     for (i=0;i<n;i++)
     {
-        scanf ("%s",word[i]);
+        scanf ("%s",&word[i]);
     }
-    for (i=1; i<n; i++)
+    for(i=0;i<n;i++)
     {
-        for (j=0; j<i; j++)
+        for(j=i+1;j<n;j++)
         {
-             while(1)
-             {
-
-                 if (word[i][k] < word[j][k])
-                 {
-                      strcpy (cha[i],word[j]);
-                      strcpy (word[j],word[i]);
-                      strcpy (word[i],cha[i]);
-                      break;
-                 }
-                 else if (word[i][k] == word[j][k])
-                 {
-                        k++;
-                 }
-                 else break;
+            if(strcmp(word[i],word[j])>0)
+            {
+                strcpy(cha,word[i]);
+                strcpy(word[i],word[j]);
+                strcpy(word[j],cha);
             }
         }
     }
